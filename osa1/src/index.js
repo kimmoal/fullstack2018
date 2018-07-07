c8a945ae7bb1f58918ed93074020e945c61c9702
Kimmo <kimmo.linnavuo@helsinki.fi>
2018-07-07 12:26:03 +0300

    1.5

--- osa1/src/index.js
+++ osa1/src/index.js
@@ -1,46 +1,48 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const Otsikko = (props) => {
+const Otsikko = ({kurssi}) => {
     return (
-        <h1>{props.kurssi}</h1>
+        <h1>{kurssi.nimi}</h1>
     )
 }
 
-const Sisalto = ({osat}) => (
-	osat.map((osa) => <Osa nimi={osa.nimi} tehtavia={osa.tehtavia} />)
+const Sisalto = ({kurssi}) => (
+	kurssi.osat.map((osa) => <Osa nimi={osa.nimi} tehtavia={osa.tehtavia} />)
 )
 
 const Osa = ({ nimi, tehtavia }) => (
 	<p>{nimi} {tehtavia}</p>
 )
 
-const Yhteensa = ({ osat }) => (
-        <p>yhteensä {osat.reduce((tehtavia, osa) => tehtavia + osa.tehtavia, 0)} tehtävää</p>
+const Yhteensa = ({ kurssi }) => (
+        <p>yhteensä {kurssi.osat.reduce((tehtavia, osa) => tehtavia + osa.tehtavia, 0)} tehtävää</p>
 )
 
 const App = () => {
-    const kurssi = 'Half Stack -sovelluskehitys'
-    const osat = [
-        {
+    const kurssi = {
+        nimi: 'Half Stack -sovelluskehitys',
+        osat: [
+          {
             nimi: 'Reactin perusteet',
             tehtavia: 10
-        },
-        {
+          },
+          {
             nimi: 'Tiedonvälitys propseilla',
             tehtavia: 7
-        },
-        {
+          },
+          {
             nimi: 'Komponenttien tila',
             tehtavia: 14
-        }
-    ]
+          }
+        ]
+    }
 
     return (
           <div>
             <Otsikko kurssi={kurssi} />
-            <Sisalto osat={osat} />
-            <Yhteensa osat={osat} />
+            <Sisalto kurssi={kurssi} />
+            <Yhteensa kurssi={kurssi} />
           </div>
         )
 }
