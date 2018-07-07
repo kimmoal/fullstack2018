16652b928aaf615ea74ed64b6c6a0c07259a7a16
Kimmo <kimmo.linnavuo@helsinki.fi>
2018-07-07 13:08:33 +0300

    Unicafe stub

---
+++ osa1/unicafe/src/index.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({header}) => (
+    <h1>{header}</h1>
+)
+
+const Button = ({clickEvent, label}) => (
+    <button onClick={clickEvent}>{label}</button>
+)
+
+const Buttons = ({feedback, clickEvent}) => (
+    feedback.map(fb => <Button key={fb.label} clickEvent={clickEvent(fb.label)} label={fb.label} />)
+)
+
+class Unicafe extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            feedback: [
+                {
+                    label: 'Hyvä',
+                    counter: 0
+                },
+                {
+                    label: 'Neutraali',
+                    counter: 0
+                },
+                {
+                    label: 'Huono',
+                    counter: 0
+                }
+            ]
+        }
+    }
+
+    add = (label) => () => {
+        let added = this.state.feedback.map(fb => {
+            if (fb.label === label) {
+                fb.counter++
+            }
+
+            return fb
+        })
+        this.setState({ feedback: added })
+    }
+
+    render = () => (
+        <div>
+            <Header header="Anna palautetta" />
+            <Buttons feedback={this.state.feedback} clickEvent={this.add} />
+            <Header header="Statistiikka" />
+        </div>
+    )
+
+}
+
+ReactDOM.render(<Unicafe />, document.getElementById('root'));
